dc44704719a7d5dbc9915f3439f1bd0ae4383ac2
oidahoch3 <132847925+oidahoch3@users.noreply.github.com>
2023-12-29 18:30:09 +0100

    finished ex1 and started ex 2

--- Gerald/G_task.m
+++ Gerald/G_task.m
@@ -5,36 +5,84 @@ clear all
 [filenames, path] = uigetfile('.mat', 'MultiSelect', 'on');
 addpath(path); % so matlab knows where my files are
 
+if ischar(filenames)
+
+    filenames = {filenames};
+
+end
+
 %% load files
 for i = 1:size(filenames, 2)
-    files{i,1} = extractBefore(filenames{i},".mat"); % erstellt eine cell array bei dem alle files abgespeichert werden. In spaltte 1 wird der name eingetragen.
-    files{i,2} = load(filenames{i}).imnData; % in spalte zwei werden die daten eingeladen.
+    
+    file = filenames{i};
+    load(file)
+
+    processed_image = process_image(imnData, 2);
+    imshow(processed_image); % displaying the image so i can select a roi
+
+    points = select_points(imnData);
+    close;
+     
+    x = linspace(900, 1600, 220); % es muss für jeden y wert einen x wert geben
+
+    tiledlayout(2, 2);
+
+    points(1)
+    nexttile
+    plot(x, points(1));
+    title ('Raw Data');
+    xlabel('Wavelength [nm]');
+    ylabel('Intensity');
+    
+    nexttile
+    for i = 1:size(fpoints, 3)
+
+        fpoints = gradient(points{i});
+        plot(x, fpoints);
+        title ('Gradient od the data');
+        xlabel('Wavelength [nm]');
+        ylabel('Intensity');
+        hold on
+
+    end
+
+    hold off
+
+    nexttile
+    title ('Mean value of the data');
+    xlabel('Wavelength [nm]');
+    ylabel('Intensity');
+    
+
+    nexttile
+    title ('Normalisation of the mean value');
+    xlabel('Wavelength [nm]');
+    ylabel('Intensity');
+    
 
 end
+
+
 
 %% processing the image
-
-for i = 1:size(files, 1)
-
-    cube = files{} % what does this cube do??
-
-
-end
-
-
-cube = files{1,2}; 
-
-processed_image = process_image(cube, 2);
-imshow(processed_image);
-
-
-
 function processed_image = process_image(image, dim)
     
     processed_image = mean(image, dim); % Mittelwert von PP
     processed_image = squeeze(processed_image); % gets rid of dim 1
-    processed_image = rot90(processed_image); % Bild rotieren
     processed_image = rescale(processed_image); % Skalierung
 
 end
- 
+
+function selected = select_points(data)    
+
+    [x, y] = ginput;
+    x = round(x);
+    y = round(y);
+
+    selected = {};
+
+    for i = 1:numel(x)
+        selected = [selected, data(y(i), :, x(i))];
+    end
+
+end
